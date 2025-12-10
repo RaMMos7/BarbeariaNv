7f1534d518c5e498b3503289989e09620a2e3a29
Miguel Ramos dos Santos <miguelramosdosantos@gmail.com>
2025-12-10 13:23:10 -0300

    0.9

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -7,4 +7,14 @@ router.post('/agendar', AgendamentoController.criar);
 router.get('/agendamentos', AgendamentoController.listar);
 router.delete('/agendamentos/:id', AgendamentoController.deletar);
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../public/index.html'));
+}   );
+
+router.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/admin-login.html'));
+});
+
+router.post('/api/agendar', agendamentoController.criar);
+
 module.exports = router;
